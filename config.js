2ebef5ec0a6813c43ac4983fb7472bd3ecdbb385
tranhungson3110 <119876026+tranhungson3110@users.noreply.github.com>
2022-12-18 23:12:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
     introTitle: 'Cảm ơn Thơ đã ấn vào link này!',
-    introDesc: `sau đây là 1 loạt câu hỏi
+    introDesc: `sau đây là câu hỏi
     tui muốn hỏi Thơ
     mong Thơ có thể trả lời được
     tất cả thắc mắc của tui`,
     btnIntro: 'OKe',
-    title: 'Tui rất vui vì Thơ đã ấn vào đây 😙',
+    title: 'Tui rất vui vì Thơ đã ấn vào đây ❤',
     desc: 'Thơ đã sẵn sàng chưa ?!! ',
     btnYes: 'Oke nha bà',
     btnNo: 'Vẫn Chưa nha bà',
     question:'Liệu tui có thể làm bạn với Thơ như bao người khác không',
     btnReply: 'Như vậy nha bà',
     reply: 'Ông có thể làm chồng tui mà',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    mess: 'Anh biết mà moah. Yêu em nhiều nhiều moah',
     messDesc: 'tui giỡn thui mong Thơ đừng giận.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
